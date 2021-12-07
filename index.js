6b5ebe8f5be217f346ce2f265e88cc71b5fd613d
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-07 13:23:52 +0000

    implementing the third and final API call which will fetch ISS flyovers for a given location.

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ const { fetchCoordsByIP } = require('./iss');
 
   console.log('It worked! Returned coordinates:' , coordinates);
 });*/
+
+/*const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});*/

ac391c987d1ac21f285c82850140e7f17757ee8c
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-07 13:18:21 +0000

    implementing the next API call which will fetch geo coordinates based on an IP address.

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+const { fetchCoordsByIP } = require('./iss');
+
+/*fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});*/
--- iss.js
+++ iss.js
@@ -20,5 +20,22 @@
     callback(null, ip);
   });
 };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
 
 module.exports = { fetchMyIP }
